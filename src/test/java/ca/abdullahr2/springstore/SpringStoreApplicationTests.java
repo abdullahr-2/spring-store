528952ab5ebcfb1eb317dbc2108b82b65e2da247
abdullahr-2 <abrlacc4578@gmail.com>
2026-05-17 22:50:28 -0600

    Initial Commit of files

---
+++ src/test/java/ca/abdullahr2/springstore/SpringStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.abdullahr2.springstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
