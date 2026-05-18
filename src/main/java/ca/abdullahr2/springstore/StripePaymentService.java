bab82e277f84e896f3c32968157d2f38f1f7528f
abdullahr-2 <abrlacc4578@gmail.com>
2026-05-17 23:22:37 -0600

    Exercise One part two

--- src/main/java/ca/abdullahr2/springstore/StripePaymentService.java
+++ src/main/java/ca/abdullahr2/springstore/StripePaymentService.java
@@ -1,4 +1,29 @@
 package ca.abdullahr2.springstore;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
+
 public class StripePaymentService implements PaymentService{
+    @Value("${stripe.apiUrl}")
+    private String apiUrl;
+
+    @Value("${stripe.timeout}")
+    private int timeout;
+
+    @Value("${stripe.enabled}")
+    private boolean enabled;
+
+    @Value("${stripe.supported-currencies}")
+    private List<String> supportedCurrencies;
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("STRIPE");
+        System.out.println("API URL: " + apiUrl);
+        System.out.println("Enabled: " + enabled);
+        System.out.println("Timeout: " + timeout);
+        System.out.println("Currencies: " + supportedCurrencies);
+        System.out.println("Amount " + amount);
+    }
 }
