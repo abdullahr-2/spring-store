943b8baae51d80a14bc99aea615a1edd7810b870
abdullahr-2 <abrlacc4578@gmail.com>
2026-05-17 23:33:18 -0600

    Exercise One done

--- src/main/java/ca/abdullahr2/springstore/SpringStoreApplication.java
+++ src/main/java/ca/abdullahr2/springstore/SpringStoreApplication.java
@@ -2,12 +2,15 @@ package ca.abdullahr2.springstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringStoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringStoreApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringStoreApplication.class, args);
+        var orderService = context.getBean(OrderService.class);
+        orderService.placeOrder();
     }
 
 }
