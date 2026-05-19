ef6ec63bfc14d6c3bd0736a222398d83c6ab438d
abdullahr-2 <abrlacc4578@gmail.com>
2026-05-19 17:53:32 -0600

    Creating profiles, user_tags, and tags tables (Exercise)

---
+++ src/main/resources/db/migration/V3__creating_profiles_table.sql
@@ -0,0 +1,12 @@
+create table profiles
+(
+    id             BIGINT                 not null
+        primary key,
+    bio            TEXT                   null,
+    phone_number   varchar(15)            null,
+    date_of_birth  DATE                   null,
+    loyalty_points INT UNSIGNED default 0 null,
+    constraint profiles_users_id_fk
+        foreign key (id) references users (id)
+);
+
---
+++ src/main/resources/db/migration/V4__creating_user_tags_and_tags_tables.sql
@@ -0,0 +1,21 @@
+-- Create the tags table
+create table tags
+(
+    id   BIGINT auto_increment
+        primary key,
+    name varchar(255) not null
+);
+
+-- Create the user_tags join table
+create table user_tags
+(
+    user_id BIGINT not null,
+    tag_id  INT    not null,
+    constraint user_tags_pk
+        primary key (user_id, tag_id),
+    constraint user_tags_tags_id_fk
+        foreign key (tag_id) references tags (id) on delete cascade,
+    constraint user_tags_users_id_fk
+        foreign key (user_id) references users (id) on delete cascade
+);
+
