1ea2b64554ae23f327d051c6dd046b87711191bf
abdullahr-2 <abrlacc4578@gmail.com>
2026-05-19 20:57:13 -0600

    Defining the user entity

--- src/main/java/ca/abdullahr2/springstore/SpringStoreApplication.java
+++ src/main/java/ca/abdullahr2/springstore/SpringStoreApplication.java
@@ -9,8 +9,6 @@ public class SpringStoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringStoreApplication.class, args);
-        var userService = context.getBean(UserService.class);
-        userService.registerUser(new User(1L, "abrlacc4578@gmail.com", "pass", "Abdullah"));
     }
 
 }
