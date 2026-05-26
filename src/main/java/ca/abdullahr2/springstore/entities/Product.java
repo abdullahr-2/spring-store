9ff1e5e7c8d6483fe636bf8913e2c54c847dcf10
abdullahr-2 <abrlacc4578@gmail.com>
2026-05-25 21:26:12 -0600

    refactoring class name to match file

--- src/main/java/ca/abdullahr2/springstore/entities/Product.java
+++ src/main/java/ca/abdullahr2/springstore/entities/Product.java
@@ -1,4 +1,24 @@
 package ca.abdullahr2.springstore.entities;
 
-public class product {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private float price;
 }
