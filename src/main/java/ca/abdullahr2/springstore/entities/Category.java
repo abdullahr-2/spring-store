83336f31ad8bf7f83e8148a7812c7e080dc6b070
abdullahr-2 <abrlacc4578@gmail.com>
2026-05-25 21:24:48 -0600

    Created basics of Product.java

--- src/main/java/ca/abdullahr2/springstore/entities/Category.java
+++ src/main/java/ca/abdullahr2/springstore/entities/Category.java
@@ -1,4 +1,21 @@
 package ca.abdullahr2.springstore.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
 }
