8d55102b43c04475affbc5b47a1b803f148c286d
abdullahr-2 <abrlacc4578@gmail.com>
2026-05-25 18:09:37 -0600

    Implementing a Many-to-One relationship between Address.java and User.java

--- src/main/java/ca/abdullahr2/springstore/SpringStoreApplication.java
+++ src/main/java/ca/abdullahr2/springstore/SpringStoreApplication.java
@@ -1,5 +1,6 @@
 package ca.abdullahr2.springstore;
 
+import ca.abdullahr2.springstore.entities.Address;
 import ca.abdullahr2.springstore.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +15,15 @@ public class SpringStoreApplication {
                 .email("abrlacc4578@gmail.com")
                 .password("pass")
                 .build();
+        Address address = Address.builder()
+                .street("street")
+                .city("city")
+                .state("state")
+                .zip("zip")
+                .build();
+
+        user.addAddress(address);
+        System.out.println(user);
     }
 
 }
